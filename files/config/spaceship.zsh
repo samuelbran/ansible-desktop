9df4d10d96a77f94d3c2004848eb8853b66c0ccd
Samuel Bran <hello@samuelbran.com>
2023-06-11 15:52:08 -0600

    ✨ Adds Oh my ZSH

---
+++ files/config/spaceship.zsh
@@ -0,0 +1,25 @@
+SPACESHIP_PROMPT_SEPARATE_LINE=false
+SPACESHIP_CHAR_SYMBOL_ROOT="#"
+
+SPACESHIP_PROMPT_ORDER=(
+  dir            # Current directory section
+  git            # Git section (git_branch + git_status)
+  char           # Prompt character
+)
+
+SPACESHIP_RPROMPT_ORDER=(
+  package        # Package version
+  node           # Node.js section
+  php            # PHP section
+  python         # Python section
+  lua            # Lua section
+  dart           # Dart section
+  docker         # Docker section
+  docker_compose # Docker section
+  ansible        # Ansible section
+  gnu_screen     # GNU Screen section
+  exec_time      # Execution time
+  jobs           # Background jobs indicator
+  exit_code      # Exit code section
+  sudo           # Sudo indicator
+)
